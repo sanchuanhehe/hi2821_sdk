cc2c30d3fca063aebd12db4062bc72b90f75eb37
sanchuanhehe <heheyoumingwyh@qq.com>
2024-08-18 14:22:21 +0000

    chore: Update SPI master task to include initialization and data transfer

--- application/blinky/blinky_cmsis.c
+++ application/blinky/blinky_cmsis.c
@@ -1,97 +1,121 @@
+/**
+ * Copyright (c) @CompanyNameMagicTag 2023-2023. All rights reserved. \n
+ *
+ * Description: SPI Sample Source. \n
+ * Author: @CompanyNameTag \n
+ * History: \n
+ * 2023-06-25, Create file. \n
+ */
 #include "app_init.h"
-#include "boards.h"
-#include "gpio.h"
-#include "osal_task.h"
+#include "cmsis_os2.h"
+#include "osal_debug.h"
 #include "pinctrl.h"
-#include "soc_osal.h"
-#include "stdint.h"
-#include "test_suite_log.h"
-#include <stdint.h>
+#include "spi.h"
 
-#define SK6812_PIN S_MGPIO11
+#define SPI_SLAVE_NUM 1
+#define SPI_FREQUENCY 2
+#define SPI_CLK_POLARITY 0
+#define SPI_CLK_PHASE 0
+#define SPI_FRAME_FORMAT 0
+#define SPI_FRAME_FORMAT_STANDARD 0
+#define SPI_FRAME_SIZE_8 0x1f
+#define SPI_TMOD 0
+#define SPI_WAIT_CYCLES 0x10
 
-// 根据CPU时钟频率和指令执行时间，调整这些常量
-#define CPU_CLOCK_MHZ 32 // 假设CPU时钟频率为48MHz
-#define NS_PER_CYCLE (1000 / CPU_CLOCK_MHZ)
+#define SPI_TASK_STACK_SIZE 0x1000
+#define SPI_TASK_DURATION_MS 500
+#define SPI_TASK_PRIO (osPriority_t)(17)
 
-#define T0H 320 // 0码，高电平时间 (单位: ns)
-#define T0L 800 // 0码，低电平时间 (单位: ns)
-#define T1H 640 // 1码，高电平时间 (单位: ns)
-#define T1L 640 // 1码，低电平时间 (单位: ns)
-#define RES 80  // Reset码，低电平时间 (单位: µs)
-
-#define BLINKY_TASK_STACK_SIZE 0x400
-#define BLINKY_TASK_PRIO OSAL_TASK_PRIORITY_ABOVE_HIGH
-
-// 简单的忙等待循环
-static void delay_ns(uint32_t ns) {
-  uint32_t cycles = ns / NS_PER_CYCLE;
-  for (volatile uint32_t i = 0; i < cycles; i++) {
-    __asm__("nop"); // 空指令，消耗时间
-  }
+static void app_spi_init_pin(void) {
+  uapi_pin_set_mode(CONFIG_SPI_DI_MASTER_PIN, CONFIG_SPI_MASTER_PIN_MODE);
+  // uapi_pin_set_mode(CONFIG_SPI_DO_MASTER_PIN, CONFIG_SPI_MASTER_PIN_MODE);
+  // uapi_pin_set_mode(CONFIG_SPI_CLK_MASTER_PIN, CONFIG_SPI_MASTER_PIN_MODE);
+  // uapi_pin_set_mode(CONFIG_SPI_CS_MASTER_PIN, CONFIG_SPI_MASTER_PIN_MODE);
 }
 
-static void sk6812_send_bit(uint8_t bit) {
-  if (bit) {
-    uapi_gpio_set_val(SK6812_PIN, GPIO_LEVEL_HIGH);
-    delay_ns(T1H); // 延时高电平T1H时间
-    uapi_gpio_set_val(SK6812_PIN, GPIO_LEVEL_LOW);
-    delay_ns(T1L); // 延时低电平T1L时间
-  } else {
-    uapi_gpio_set_val(SK6812_PIN, GPIO_LEVEL_HIGH);
-    delay_ns(T0H); // 延时高电平T0H时间
-    uapi_gpio_set_val(SK6812_PIN, GPIO_LEVEL_LOW);
-    delay_ns(T0L); // 延时低电平T0L时间
-  }
+static void app_spi_master_init_config(void) {
+  spi_attr_t config = {0};
+  spi_extra_attr_t ext_config = {0};
+
+  config.is_slave = false;
+  config.slave_num = SPI_SLAVE_NUM;
+  config.bus_clk = SPI_CLK_FREQ;
+  config.freq_mhz = SPI_FREQUENCY;
+  config.clk_polarity = SPI_CLK_POLARITY;
+  config.clk_phase = SPI_CLK_PHASE;
+  config.frame_format = SPI_FRAME_FORMAT;
+  config.spi_frame_format = HAL_SPI_FRAME_FORMAT_STANDARD;
+  config.frame_size = SPI_FRAME_SIZE_8;
+  config.tmod = SPI_TMOD;
+  config.sste = 1;
+
+  ext_config.qspi_param.wait_cycles = SPI_WAIT_CYCLES;
+  uapi_dma_init();
+  uapi_dma_open();
+  uapi_spi_init(CONFIG_SPI_MASTER_BUS_ID, &config, &ext_config);
 }
 
-static void sk6812_send_byte(uint8_t byte) {
-  for (int i = 0; i < 8; i++) {
-    sk6812_send_bit((byte << i) & 0x80); // 发送最高位开始的每一位
-  }
-}
-
-static void sk6812_reset(void) {
-  uapi_gpio_set_val(SK6812_PIN, GPIO_LEVEL_LOW);
-  osal_msleep(RES); // 延时Reset低电平时间
-}
-
-static void *sk6812_task(const char *arg) {
+static void *spi_master_task(const char *arg) {
   unused(arg);
+  /* SPI pinmux. */
+  app_spi_init_pin();
 
-  uapi_pin_set_mode(SK6812_PIN, HAL_PIO_FUNC_GPIO);
-  uapi_gpio_set_dir(SK6812_PIN, GPIO_DIRECTION_OUTPUT);
-  uapi_gpio_set_val(SK6812_PIN, GPIO_LEVEL_LOW);
+  /* SPI master init config. */
+  app_spi_master_init_config();
+
+  /* SPI data config. */
+  uint8_t tx_data[CONFIG_SPI_TRANSFER_LEN] = {0};
+  for (uint32_t loop = 0; loop < CONFIG_SPI_TRANSFER_LEN; loop++) {
+    tx_data[loop] = (loop & 0xFF);
+  }
+  uint8_t rx_data[CONFIG_SPI_TRANSFER_LEN] = {0};
+  spi_xfer_data_t data = {
+      .tx_buff = tx_data,
+      .tx_bytes = CONFIG_SPI_TRANSFER_LEN,
+      .rx_buff = rx_data,
+      .rx_bytes = CONFIG_SPI_TRANSFER_LEN,
+  };
 
   while (1) {
-    // 发送SK6812数据
-    sk6812_send_byte(0x00); // 示例: 全亮红色
-    sk6812_send_byte(0x00); // 示例: 绿色关闭
-    sk6812_send_byte(0x00); // 示例: 蓝色关闭
-    sk6812_reset();         // 发送完数据后进行Reset
-    osal_msleep(1000);      // 延时一段时间后再发送数据
+    osDelay(SPI_TASK_DURATION_MS);
+    osal_printk("spi%d master send start!\r\n", CONFIG_SPI_MASTER_BUS_ID);
+    if (uapi_spi_master_write(CONFIG_SPI_MASTER_BUS_ID, &data, 0xFFFFFFFF) ==
+        ERRCODE_SUCC) {
+      osal_printk("spi%d master send succ!\r\n", CONFIG_SPI_MASTER_BUS_ID);
+    } else {
+      continue;
+    }
+    osal_printk("spi%d master receive start!\r\n", CONFIG_SPI_MASTER_BUS_ID);
+    if (uapi_spi_master_read(CONFIG_SPI_MASTER_BUS_ID, &data, 0xFFFFFFFF) ==
+        ERRCODE_SUCC) {
+#ifndef CONFIG_SPI_SUPPORT_INTERRUPT
+      for (uint32_t i = 0; i < data.rx_bytes; i++) {
+        osal_printk("spi%d master receive data is %x\r\n",
+                    CONFIG_SPI_MASTER_BUS_ID, data.rx_buff[i]);
+      }
+#endif
+      osal_printk("spi%d master receive succ!\r\n", CONFIG_SPI_MASTER_BUS_ID);
+    }
   }
+
   return NULL;
 }
 
-static void sk6812_entry(void) {
-  uint32_t ret;
-  osal_task *taskid;
+static void spi_master_entry(void) {
+  osThreadAttr_t attr;
 
-  // 创建任务调度
-  osal_kthread_lock();
+  attr.name = "SpiMasterTask";
+  attr.attr_bits = 0U;
+  attr.cb_mem = NULL;
+  attr.cb_size = 0U;
+  attr.stack_mem = NULL;
+  attr.stack_size = SPI_TASK_STACK_SIZE;
+  attr.priority = SPI_TASK_PRIO;
 
-  // 创建任务
-  taskid = osal_kthread_create((osal_kthread_handler)sk6812_task, NULL,
-                               "sk6812_task", BLINKY_TASK_STACK_SIZE);
-
-  // 设置任务优先级
-  ret = osal_kthread_set_priority(taskid, BLINKY_TASK_PRIO);
-  if (ret != OSAL_SUCCESS) {
-    printf("create task failed.\n");
+  if (osThreadNew((osThreadFunc_t)spi_master_task, NULL, &attr) == NULL) {
+    /* Create task fail. */
   }
-  osal_kthread_unlock();
 }
 
-/* Run the sk6812_entry. */
-app_run(sk6812_entry);
+/* Run the spi_master_entry. */
+app_run(spi_master_entry);
